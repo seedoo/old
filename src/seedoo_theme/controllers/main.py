0df7b387f46c40cf204208c739066009d94deb1d
archetipo <alessio.gerace@gmail.com>
2015-12-10 18:31:28 +0100

    [FIX] controller main.py [FIX] label color of user and pass [FIX] add link to seedoo logo

--- src/seedoo_theme/controllers/main.py
+++ src/seedoo_theme/controllers/main.py
@@ -3,28 +3,6 @@
 # this module contains the full copyright notices and license terms.
 
 from openerp.addons.web import controllers
-controllers.main.html_template = u"""<!DOCTYPE html>
-<html style="height: 100%%">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title>Seedoo</title>
-        <link rel="shortcut icon" href="/seedoo_theme/static/src/img/favicon.ico" type="image/x-icon"/>
-        <link rel="stylesheet" href="/web/static/src/css/full.css" />
-        %(css)s
-        %(js)s
-        <script type="text/javascript">
-            $(function() {
-                var s = new openerp.init(%(modules)s);
-                %(init)s
-            });
-        </script>
-    </head>
-    <body>
-        <!--[if lte IE 8]>
-        <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
-        <script>CFInstall.check({mode: "overlay"});</script>
-        <![endif]-->
-    </body>
-</html>
-"""
+controllers.main.html_template = controllers.main.html_template.replace(
+    "/web/static/src/img/favicon.ico", "/seedoo_theme/static/src/img/favicon.ico"
+    ).replace("<title>OpenERP</title>", "<title>Seedoo</title>")
