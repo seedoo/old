eca73d8394f72d60e067fbcd0335e3ce7375c455
angedras <roberto.onnis@innoviu.com>
2016-07-27 10:25:59 +0200

    [seedoo_protocollo]: adds protocollo search for gedoc

--- src/seedoo_protocollo/gedoc/gedoc.py
+++ src/seedoo_protocollo/gedoc/gedoc.py
@@ -46,3 +46,49 @@ class protocollo_dossier(osv.Model):
                 return True
         return super(protocollo_dossier, self).\
             is_document_present(cr, uid, ids, *args)
+
+
+class DocumentSearch(osv.TransientModel):
+    """
+        Advanced Document Search
+    """
+    _inherit = 'gedoc.document.search'
+
+    def _search_action_document(
+            self, cr, uid, wizard,
+            search_domain, context=None):
+        if wizard.name == 'protocollo.protocollo':
+            if wizard.text_name:
+                search_domain.append(('name', 'ilike', wizard.text_name))
+            if wizard.subject:
+                search_domain.append(('subject', 'ilike', wizard.subject))
+            if wizard.classification_id:
+                search_domain.append(
+                    ('classification', '=', wizard.classification_id.id))
+            if wizard.partner_id:
+                search_domain.append(
+                    ('sender_receivers',
+                     'ilike',
+                     wizard.partner_id.name)
+                    )
+            if wizard.office_id:
+                search_domain.append(
+                    ('assigne',
+                     'in',
+                     [wizard.office_id.id])
+                    )
+            if wizard.date_close_start:
+                search_domain.append(
+                    ('registration_date',
+                     '>=',
+                     wizard.date_close_start)
+                    )
+            if wizard.date_close_end:
+                search_domain.append(
+                    ('registration_date',
+                     '<=',
+                     wizard.date_close_end)
+                    )
+            return search_domain
+        else:
+            return search_domain
